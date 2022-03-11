c2957269932947c97bf7e18d4bf9eb99e58e49b3
Noah Kirchner <noahthomkirchner@gmail.com>
2022-03-11 16:04:26 -0500

    adds a ton of stuff
    dont have time for descriptontiotnotn

--- startenum.sh
+++ startenum.sh
@@ -17,35 +17,62 @@ filepath=~/hosts/$dirname
 
 touch $filepath/users.out
 userspath=$filepath/users.out
-echo "===USER INFORMATION @ $dirname===\n\n"
-echo "==Currently Online Users==\n" >> $userspath
+echo -e  "\n===USER INFORMATION @ $dirname===\n\n" > $userspath
+echo -e  "\n==Currently Online Users==\n" >> $userspath
 w >> $userspath
-echo "==User Login History==\n" >> $userspath
+echo -e  "\n==User Login History==\n" >> $userspath
 last >> $userspath
-echo "==/etc/passwd==\n" >> $userspath
+echo -e  "\n==/etc/passwd==\n" >> $userspath
 cat /etc/passwd >> $userspath
-echo "==/etc/groups==\n" >> $userspath
-cat /etc/groups >> $userspath
-echo "==/etc/sudoers==\n" >> $userspath
+echo -e  "\n==/etc/groups==\n" >> $userspath
+cat /etc/group >> $userspath
+echo -e  "\n==/etc/sudoers==\n" >> $userspath
 sudo cat /etc/sudoers >> $userspath
-
-
+echo -e "\n==========================\n"
 
 
 # System Information
+touch $filepath/host.out
+hostpath=$filepath/host.out
+
+echo -e  "\n===HOST INFORMATION @ $dirname===\n\n" > $hostpath
+
+echo -e  "\n==HOSTNAME==\n" >> $userspath
+hostname
+echo -e  "\n==VERSION INFORMATION==\n" >> $userspath
+uname -a
+echo -e  "\n==RELEASE INFORMATION==\n" >> $userspath
+cat /etc/*rel*
+echo -e  "\n==SYSTEM UPTIME==\n" >> $userspath
+uptime
 
 
 
 # Persistence
+touch $filepath/persistence.out
+perpath=$filepath/persistence.out
+
 
 
 
 # Network
+touch $filepath/network.out
+netpath=$filepath/network.out
+
 
 
 
 # Processes
+touch $filepath/process.out
+procpath=$filepath/process.out
+
 
 
 
 # File Enumeration
+touch $filepath/files.out
+filenumpath=$filepath/files.out
+
+
+
+
